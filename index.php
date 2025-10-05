b46a4e6b8c7a8d9fef3eeaf03e6e685f454c684b
MayraS777 <mayrasofiaa3664@gmail.com>
2025-10-05 16:39:31 -0300

    Modificacion en datos
    Modificamos la forma de mostrar los datos para simplificar el codigo

--- index.php
+++ index.php
@@ -1,25 +1,49 @@
 <?php
-function getImage($name): string {
-$images = array ( 
-"https://w7.pngwing.com/pngs/538/981/png-transparent-bmw-logo-bmw-car-logo-bmw-logo-trademark-logo-graphics.png",
-"https://w7.pngwing.com/pngs/129/888/png-transparent-mercedes-benz-sprinter-car-mercedes-benz-c-class-luxury-vehicle-benz-logo-angle-trademark-triangle.png",
-"https://w7.pngwing.com/pngs/905/811/png-transparent-audi-q5-car-logo-audi-a3-audi-brand-text-vehicle-map-thumbnail.png",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMsxaDxFlD8rE_ah9GXtvWxxvK8sAX6WRsbQ&s",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO9KO5EhBdNIBdA9xgAQ971I0KTwc6dh1IoQ&s",
-"https://cdn.imgbin.com/24/16/1/imgbin-honda-logo-car-honda-city-honda-accord-honda-silver-honda-emblem-wAR2UeNMsA9rJw8BG1FcD6Vpj.jpg",
-"https://w7.pngwing.com/pngs/579/799/png-transparent-alfa-romeo-romeo-car-alfa-romeo-giulia-alfa-romeo-spider-alfa-romeo-emblem-trademark-logo.png",
-"https://i.pinimg.com/736x/11/ee/ec/11eeec868f528be972314d1f07ac212f.jpg",
-);
-if($name == "bmw") return $images[0]; 
-if($name == "mercedes") return $images[1];
-if($name == "audi") return $images[2];
-if($name == "volkswagen") return $images[3];
-if($name == "peugeot") return $images[4];
-if($name == "honda") return $images[5];
-if($name == "alfaromeo") return $images[6];
-if($name == "jeep") return $images[7];
-return "https://www.valuehost.com.br/blog/wp-content/uploads/2022/01/post_thumbnail-77d8f2a95f2f41b5863f3fba5a261d7e.jpeg.webp";
-}
+
+$bmw = (object) [
+	'name' => 'BMW',
+	'url' => 'https://w7.pngwing.com/pngs/538/981/png-transparent-bmw-logo-bmw-car-logo-bmw-logo-trademark-logo-graphics.png',
+	'link' => 'https://www.bmw.com.ar/es/all-models.html'
+];
+$mercedes = (object) [
+	'name' => 'MERCEDES BENZ',
+	'url' => 'https://w7.pngwing.com/pngs/129/888/png-transparent-mercedes-benz-sprinter-car-mercedes-benz-c-class-luxury-vehicle-benz-logo-angle-trademark-triangle.png',
+	'link' => 'https://www.mercedes-benz.com.ar/passengercars/models.html?group=all&subgroup=see-all&filters='
+];
+$audi = (object) [
+	'name' => 'AUDI',
+	'url' => 'https://w7.pngwing.com/pngs/905/811/png-transparent-audi-q5-car-logo-audi-a3-audi-brand-text-vehicle-map-thumbnail.png',
+	'link' => 'https://www.audi.com.ar/es/modelos/'
+];
+$volkswagen = (object) [
+	'name' => 'VOLKSWAGEN',
+	'url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMsxaDxFlD8rE_ah9GXtvWxxvK8sAX6WRsbQ&s',
+	'link' => 'https://www.volkswagen.com.ar/es/modelos.html'
+];
+$peugeot = (object) [
+	'name' => 'PEUGEOT',
+	'url' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTO9KO5EhBdNIBdA9xgAQ971I0KTwc6dh1IoQ&s',
+	'link' => 'https://www.peugeot.com.ar/modelos/peugeot-gama.html'
+];
+$honda = (object) [
+	'name' => 'HONDA',
+	'url' => 'https://cdn.imgbin.com/24/16/1/imgbin-honda-logo-car-honda-city-honda-accord-honda-silver-honda-emblem-wAR2UeNMsA9rJw8BG1FcD6Vpj.jpg',
+	'link' => 'https://autos.honda.com.ar/'
+];
+$alfaromeo = (object) [
+	'name' => 'ALFAROMEO',
+	'url' => 'https://w7.pngwing.com/pngs/579/799/png-transparent-alfa-romeo-romeo-car-alfa-romeo-giulia-alfa-romeo-spider-alfa-romeo-emblem-trademark-logo.png',
+	'link' => 'https://www.alfaromeo.com.ar/'
+];
+$jeep = (object) [
+	'name' => 'JEEP',
+	'url' => 'https://i.pinimg.com/736x/11/ee/ec/11eeec868f528be972314d1f07ac212f.jpg',
+	'link' => 'https://www.jeep.com.ar/'
+];
+
+$marcas = [$bmw, $mercedes, $audi, $volkswagen, $peugeot, $honda, $alfaromeo, $jeep];
+
+$cards = count(value: $marcas);
 
 $cssEstilos = "estilos";
 $tituloppal =  "Consecionaria 22";
@@ -37,91 +61,16 @@ $descriptitulo = "Donde compran los locos por los autos";
 <body>
 <h1 class="tituloppal"> <?= $tituloppal; ?> </h1>
 <h2> <?= $descriptitulo;?> </h2>
-
-<div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="<?= getImage("bmw"); ?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">BMW</h5>
-        <p class="card-text">Examina nuestros modelos más recientes para encontrar la tecnología más inteligente, funciones innovadoras y los tipos de motores que mejor se adaptan a ti. Compara, configura y encuentra tu nuevo BMW perfecto.</p>
-		<a href="#" class="btn btn-primary">Ver Modelos</a>
-	</div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="<?= getImage("mercedes"); ?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">MERCEDES BENZ</h5>
-        <p class="card-text">Descubrí nuestros modelos más recientes. La combinación perfecta de lujo, deportividad y potencia.</p>
-		<a href="#" class="btn btn-primary">Ver Modelos</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="<?= getImage("audi"); ?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">AUDI</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-		<a href="#" class="btn btn-primary">Ver Modelos</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="<?= getImage("volkswagen"); ?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">VOLKSWAGEN</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-		<a href="#" class="btn btn-primary">Ver Modelos</a>
-      </div>
-    </div>
+<?php for($i = 0; $i < $cards; $i++) { ?>
+<div class="card" style="width: 18rem;">
+  <img src="<?= $marcas[$i] -> url ?>" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><?= $marcas[$i] -> name ?> </h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="<?= $marcas[$i] -> link ?>" class="btn btn-primary">Ver Modelos</a>
   </div>
 </div>
-<div class="row row-cols-1 row-cols-md-2 g-4">
-  <div class="col">
-    <div class="card">
-      <img src="<?= getImage("peugeot"); ?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">PEUGEOT</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-		<a href="#" class="btn btn-primary">Ver Modelos</a>
-	</div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="<?= getImage("honda"); ?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">HONDA</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-		<a href="#" class="btn btn-primary">Ver Modelos</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="<?= getImage("alfaromeo"); ?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">ALFAROMEO</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-		<a href="#" class="btn btn-primary">Ver Modelos</a>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card">
-      <img src="<?= getImage("jeep"); ?>" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title">JEEP</h5>
-        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-		<a href="#" class="btn btn-primary">Ver Modelos</a>
-      </div>
-    </div>
-  </div>
-</div>
+<?php } ?>
 	
 </body>
 </html>
